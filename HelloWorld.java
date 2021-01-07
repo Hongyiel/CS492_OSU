4ed117bfd7afc60e61fc0c02779ec50886fc8e6c
Hongyiel <suhho@oregonstate.edu>
2021-01-08 07:54:58 +0900

    01/08

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,9 +7,25 @@ class HelloWorld {
     short sh = 16;
     int i = 32;
     long l = 64;
-    float f = 32.0;
+    float f = (float)32.0;
+    // float f = 32.0f;
     double d = 64.0;
-    System.out.println("Hello, world!");
 
+    System.out.println("Hello, world!");
+    // no memory allocate types
+    // int[] arr;
+    // allocate array
+    int[] arr = new int[10];
+    arr[0] = 0;
+    // arr[1000] = 1000;
+    for (int j=0; j< arr.length; j++){
+      arr[j] = j * j;
+    }
+
+    for (int elem : arr){
+      System.out.println(elem);
+    }
+
+    int[] arr2 = arr;
   }
 }
